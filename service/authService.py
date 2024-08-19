682a16f53ddacaf60548d1d672a8754e1d0d4ebb
Pourya Samimi <neverdiegenius@gmail.com>
2024-08-19 22:07:23 +0330

    Add:L create delete users functions

--- service/authService.py
+++ service/authService.py
@@ -1,11 +1,11 @@
 from repository.authRepository import UserRepository
 from sqlalchemy.ext.asyncio import AsyncSession
-from schema.inputs import RegisterInputs, UpdateUsernameInput
+from schema.inputs import RegisterInputs, UpdateUsernameInput, DeleteUserInput
 from model.usersModel import User
 
 
 class UserService:
-    def __init__(self, data: RegisterInputs | UpdateUsernameInput, session: AsyncSession):
+    def __init__(self, data: RegisterInputs | UpdateUsernameInput | DeleteUserInput, session: AsyncSession):
         self.repository = UserRepository(db_session=session, data=data)
 
     async def create(self) -> bool:
@@ -15,3 +15,7 @@ class UserService:
     async def update(self) -> User:
         user = await self.repository.update_username()
         return user
+
+    async def delete(self) -> bool:
+        status = await self.repository.delete_user()
+        return status
