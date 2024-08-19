089622717e1a979a1f0bbe0fbeff09fc701ed5f6
Pourya Samimi <neverdiegenius@gmail.com>
2024-08-19 21:20:28 +0330

    Add: create users routes

---
+++ route/users.py
@@ -0,0 +1,27 @@
+from fastapi import APIRouter, Body, Depends
+from typing import Annotated
+from sqlalchemy.ext.asyncio import AsyncSession
+from schema.inputs import RegisterInputs, UpdateUsernameInput
+from database.engine import get_db
+from service.authService import UserService
+
+user_router: APIRouter = APIRouter()
+
+
+@user_router.post("/signup")
+async def signup(db_session: Annotated[AsyncSession, Depends(get_db)], data: RegisterInputs = Body()):
+    service = UserService(data=data, session=db_session)
+    status = await service.create()
+    return status
+
+
+@user_router.post("/signin")
+async def signin():
+    ...
+
+
+@user_router.put("/update")
+async def update(db_session: Annotated[AsyncSession, Depends(get_db)], data: UpdateUsernameInput = Body()):
+    service = UserService(data=data, session=db_session)
+    user = await service.update()
+    return user
