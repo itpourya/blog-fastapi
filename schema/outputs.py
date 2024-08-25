5c9f2650fbc12d14e6497f9f2bafe58b82f47b12
Pourya Samimi <neverdiegenius@gmail.com>
2024-08-25 20:00:05 +0330

    Add: create output for get user and jwt payload

--- schema/outputs.py
+++ schema/outputs.py
@@ -5,3 +5,20 @@ from uuid import UUID
 class RegisterOutput(BaseModel):
     username: str
     id: UUID
+
+
+class GetUserOutput(BaseModel):
+    username: str
+    id: UUID
+    first_name: str
+    last_name: str
+    password: str
+    active: bool
+    email: str
+
+
+class JWTPayload(BaseModel):
+    username: str
+    email: str
+    id: str
+    exp: int
